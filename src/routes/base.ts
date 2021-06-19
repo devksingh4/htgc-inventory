430f9aa8d81f35575d0ab5415bf68a14c9b6e301
Dev Singh <dev@devksingh.com>
2021-06-18 21:24:07 -0500

    add functioning thing

--- src/routes/base.ts
+++ src/routes/base.ts
@@ -3,6 +3,12 @@ import { v4 as uuidv4 } from 'uuid';
 module.exports = (app) => {
   app.get('/', async (req, res) => {
     const groups = [{ name: 'Kitchen', id: '9458f755-f6c0-4001-902c-8e2adb3bae7b' }, { name: 'Front Desk', id: '30528543-b245-477f-925b-72638c446511' }]
-    res.render('index', { uuid: uuidv4(), groupLength: groups.length > 1, groups, units: ['box', 'kg', 'lb', 'g'] })
+    res.render('index', { 
+      uuid: uuidv4(),
+      groupLength: groups.length > 1,
+      groups, 
+      units: ['box', 'kg', 'lb', 'g'],
+      user: { picture: 'https://devksingh.com/images/me.jpg', name: 'Dev Singh' } 
+    })
   })
 }
